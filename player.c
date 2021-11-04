54635d20b8cc0e01bce3882fc3788c1fa6184bc2
doniatung <donia.tung@dali.dartmouth.edu>
2021-11-04 17:45:18 -0400

    setters and getters written maybe

--- player.c
+++ player.c
@@ -14,6 +14,13 @@
 #include <stdbool.h>
 // #include "grid.h"
 
+/**************** file-local global variables ****************/
+/* none */
+
+/**************** local types ****************/
+/* none */
+
+/**************** global types ****************/
 typedef struct player{
     char* realName; 
     char* ID; 
@@ -22,3 +29,52 @@ typedef struct player{
     int gold; 
     bool spectator; 
 } player_t; 
+
+typedef struct tuple{
+    int x; 
+    int y; 
+} tuple_t; 
+
+/**************** global functions ****************/
+/* that is, visible outside this file */
+/* see player.h for comments about exported functions */
+
+/**************** local functions ****************/
+/* not visible outside this file */
+// TODO: init here as necessary 
+
+/**************** getRealName ****************/
+char* getRealName(player_t* player) { return player->realName; }
+
+/**************** setRealName ****************/
+void setRealName(player_t* player, char* realName) { player->realName = realName; }
+
+/**************** getID ****************/
+char* getID(player_t* player) { return player->ID; }
+
+/**************** setID ****************/
+void setID(player_t* player, char* ID) { player->ID = ID; }
+
+/**************** getGrid ****************/
+grid_t* getGrid(player_t* player) { return player->grid; }
+
+/**************** getGrid ****************/
+void getGrid(player_t* player, grid_t* grid) { player->grid = grid; }
+
+/**************** getCurrentPos ****************/
+tuple_t* getCurrentPos(player_t* player) { return player->currentPosition; }
+
+/**************** setCurrentPos ****************/
+void setCurrentPos(player_t* player, tuple_t* currentPosition) { player->currentPosition = currentPosition; } 
+
+/**************** getGold ****************/
+int getGold(player_t* player) { return player->gold; }
+
+/**************** setGold ****************/
+void setGold(player_t* player, int gold) { player->gold = gold; }
+
+/**************** getSpectatorStatus ****************/
+bool getSpectatorStatus(player_t* player) { return player->spectator; }
+
+/**************** setSpectatorStatus ****************/
+void setSpectatorStatus(player_t* player, bool spectator) { player->spectator = spectator; }
