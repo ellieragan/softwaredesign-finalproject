aa7ecf5264c4332b5ba04960a60f46174afc23c2
Donia Tung <donia.y.tung.22@dartmouth.edu>
2021-11-14 21:18:39 -0500

    removing comments

--- player.c
+++ player.c
@@ -80,11 +80,10 @@ player_t* initPlayer(char* realName, const char ID, grid_t* masterGrid, const ad
 
     // randomly select a position for the player
     tuple_t* currentPos = getRandomPosition(masterGrid); 
-    printf("position: %d, %d\n", tupleGetX(currentPos), tupleGetY(currentPos)); 
 
     // calculate player's visibility at that position 
     char* visibility = initializeVisibility(masterGrid, tupleGetY(currentPos),  tupleGetX(currentPos)); 
-    printf("visibility: %s\n", visibility);
+
     // allocate space and set instance variables
     player_t* player = mem_malloc(sizeof(player_t));
     setRealName(player, realName); 
@@ -109,7 +108,6 @@ tuple_t* getRandomPosition(grid_t* grid)
 
     int numRows = getRows(grid); 
     int numCols = getCols(grid); 
-    printf("rows: %d, cols: %d", numRows, numCols); 
 
     int x = rand() % (numRows + 1); 
     int y = rand() % (numCols + 1); 
